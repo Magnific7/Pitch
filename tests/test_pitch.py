c84dbc147abff5781aa454f8f70e29b2c8f06bf5
UM Magnific <52447740+Magnific7@users.noreply.github.com>
2019-09-26 17:52:24 +0200

    Update test_pitch.py

--- tests/test_pitch.py
+++ tests/test_pitch.py
@@ -31,11 +31,3 @@ class PitchTest(unittest.TestCase):
         self.new_pitch.save_pitch()
         got_pitches = Pitch.get_pitch(30)
         self.assertTrue(len(got_pitches) == 1)
-
-    # def test_check_instance_variables(self):
-    #     self.assertEquals(self.new_pitch.id,30)
-    #     self.assertEquals(self.new_pitch.pitch_title,'movie')
-    #     self.assertEquals(self.new_pitch.content,'Watch moremovies')
-    #     self.assertEquals(self.new_pitch.category,"Product Pitch")
-    #     self.assertEquals(self.new_pitch.user_id,self.user_Sophy)
-    #     self.assertEquals(self.new_pitch.comments,self.new_comment)    
